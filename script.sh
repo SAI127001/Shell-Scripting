367125bcf73bb0bb9854d4ecf240a34a47fd81b5
sai127001 <codesai127.0.0.1@gmail.com>
2024-12-17 00:17:56 +0530

    Practise

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p DevOps/Basic_Tools
+mkdir -p DevOps/Basic_Tools/Git
+mkdir -p DevOps/Basic_Tools/Linux
+mkdir -p DevOps/Basic_Tools/Shell_Script
+
+mkdir -p DevOps/Advance_Tools
+mkdir -p DevOps/Advance_Tools/Maven
+mkdir -p DevOps/Advance_Tools/Jenkins
+mkdir -p DevOps/Advance_Tools/Docker
+mkdir -p DevOps/Advance_Tools/Kubernetes
+
+echo "Created DevOps hierarchy sucessfully........!"
+tree DevOps
+
